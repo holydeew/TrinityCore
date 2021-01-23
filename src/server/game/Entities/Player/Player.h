f3ff347d00d699b787db7096e9c0440d210541c2
Matan Shukry <matanshukry@gmail.com>
2021-01-24 01:09:04 +0200

    - Removed extra xp & currency. They're automatically handled with   effects & auras - Using different aura if player is at max level

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2549,8 +2549,6 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         bool IsWarModeDesired() const { return HasPlayerFlag(PLAYER_FLAGS_WAR_MODE_DESIRED); }
         bool IsWarModeActive() const { return HasPlayerFlag(PLAYER_FLAGS_WAR_MODE_ACTIVE); }
         void UpdateWarModeAuras();
-        bool HasWarmodeEnlistedAura() const { return HasAura(269083); }
-        float GetWarmodeAuraMod() const { return 1.1f; }
 
         UF::UpdateField<UF::PlayerData, 0, TYPEID_PLAYER> m_playerData;
         UF::UpdateField<UF::ActivePlayerData, 0, TYPEID_ACTIVE_PLAYER> m_activePlayerData;
